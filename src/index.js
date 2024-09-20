7bb38278aa565adc51ecb1e3a031d20574377421
Adam Hussain <adam@hussmedia.io>
2024-09-20 13:14:41 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const BookList = () => {
+  return (
+    <section className="bookList">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => <img src="./images/book-1.jpg" alt="Key Lime Garden Inn" />;
+const Title = () => <h2>Key Lime Garden Inn</h2>;
+const Author = () => {
+  return (
+    <h4 style={{ color: "#617d198", fontSize: "0.75rem", marginTop: "0.5rem" }}>
+      Annie Cabot
+    </h4>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
+
+
